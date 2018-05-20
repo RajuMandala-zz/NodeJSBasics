997d0ec5cd02b21206c29612c38ab878849f2c18
Raju Mandala <raju@Rajus-MacBook-Pro.local>
2018-05-20 13:00:48 +0530

    server with timeout

---
+++ 01-node-first-app/server-setTimeout.js
@@ -0,0 +1,14 @@
+//We meed http module for web app
+var http = require('http');
+
+//Create Server and listen on port 8001
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});	
+	response.write('Hello\n');
+	setTimeout(function(){
+		response.end('node\n');
+	}, 20000);
+});
+
+server.listen(8001);
+
