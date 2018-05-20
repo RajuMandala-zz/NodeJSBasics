de91bac4033bbee496f7d91ac37bfce4f2f932de
Raju Mandala <raju@Rajus-MacBook-Pro.local>
2018-05-20 12:42:23 +0530

    server js code reformatting

--- 01-node-first-app/server.js
+++ 01-node-first-app/server.js
@@ -2,11 +2,11 @@
 var http = require('http');
 
 //Create Server and listen on port 8001
-http.createServer(onRequest).listen(8001);
-
-//Handling Request
-function onRequest(request, response) {
+var server = http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Hello Node');
+	response.write('Hello Node\n');
 	response.end();
-}
+});
+
+server.listen(8001);
+
